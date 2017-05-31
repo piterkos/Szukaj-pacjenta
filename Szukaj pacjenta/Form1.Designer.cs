9a17c130c2e427d5adbf289d3395f1356bc86eca
Piotr <piterpaw@gmail.com>
2017-05-31 19:34:27 +0200

    wstępna wersja gotowa

--- Szukaj pacjenta/Form1.Designer.cs
+++ Szukaj pacjenta/Form1.Designer.cs
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cboArkusze = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_pesel = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbox_wyfiltrowaneDane = new System.Windows.Forms.RichTextBox();
+            this.lbl_danePacjenta = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(314, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(581, 338);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cboArkusze
+            // 
+            this.cboArkusze.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboArkusze.FormattingEnabled = true;
+            this.cboArkusze.Location = new System.Drawing.Point(314, 38);
+            this.cboArkusze.Name = "cboArkusze";
+            this.cboArkusze.Size = new System.Drawing.Size(121, 21);
+            this.cboArkusze.TabIndex = 2;
+            this.cboArkusze.SelectedIndexChanged += new System.EventHandler(this.cboArkusze_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Otwórz plik excela z bazą";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_pesel
+            // 
+            this.textBox_pesel.Location = new System.Drawing.Point(12, 106);
+            this.textBox_pesel.Name = "textBox_pesel";
+            this.textBox_pesel.Size = new System.Drawing.Size(196, 20);
+            this.textBox_pesel.TabIndex = 4;
+            this.textBox_pesel.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_pesel_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Szukaj badania wg nr PESEL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(442, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Wybierz rok do wyświetlenia";
+            // 
+            // rtbox_wyfiltrowaneDane
+            // 
+            this.rtbox_wyfiltrowaneDane.Location = new System.Drawing.Point(13, 189);
+            this.rtbox_wyfiltrowaneDane.Name = "rtbox_wyfiltrowaneDane";
+            this.rtbox_wyfiltrowaneDane.Size = new System.Drawing.Size(295, 214);
+            this.rtbox_wyfiltrowaneDane.TabIndex = 9;
+            this.rtbox_wyfiltrowaneDane.Text = "";
+            // 
+            // lbl_danePacjenta
+            // 
+            this.lbl_danePacjenta.AutoSize = true;
+            this.lbl_danePacjenta.Location = new System.Drawing.Point(12, 170);
+            this.lbl_danePacjenta.Name = "lbl_danePacjenta";
+            this.lbl_danePacjenta.Size = new System.Drawing.Size(80, 13);
+            this.lbl_danePacjenta.TabIndex = 10;
+            this.lbl_danePacjenta.Text = "Dane pacjenta:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.opisToolStripMenuItem,
+            this.autorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(918, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zakończToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            this.zakończToolStripMenuItem.Click += new System.EventHandler(this.zakończToolStripMenuItem_Click);
+            // 
+            // opisToolStripMenuItem
+            // 
+            this.opisToolStripMenuItem.Name = "opisToolStripMenuItem";
+            this.opisToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.opisToolStripMenuItem.Text = "Opis";
+            this.opisToolStripMenuItem.Click += new System.EventHandler(this.opisToolStripMenuItem_Click);
+            // 
+            // autorToolStripMenuItem
+            // 
+            this.autorToolStripMenuItem.Name = "autorToolStripMenuItem";
+            this.autorToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.autorToolStripMenuItem.Text = "Autor";
+            this.autorToolStripMenuItem.Click += new System.EventHandler(this.autorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(918, 457);
+            this.Controls.Add(this.lbl_danePacjenta);
+            this.Controls.Add(this.rtbox_wyfiltrowaneDane);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox_pesel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cboArkusze);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Szukaj pacjenta";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cboArkusze;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_pesel;
+        private System.Windows.Forms.Button btn_szykajWgPESEL;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbox_wyfiltrowaneDane;
+        private System.Windows.Forms.Label lbl_danePacjenta;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autorToolStripMenuItem;
     }
 }
 
--- Szukaj pacjenta/Form1.cs
+++ Szukaj pacjenta/Form1.cs
@@ -7,14 +7,174 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExcelInter = Microsoft.Office.Interop.Excel;
+using System.IO;
+using Excel;
 
 namespace Szukaj_pacjenta
 {
     public partial class Form1 : Form
     {
+        DataSet wynik;
+        bool CzyZaładowanoPlik = false;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog() {
+                Filter = "Pliki Excel (*.xls)|*.xls|Pliki Excel (*.xlsx)|*.xlsx", FilterIndex = 2})
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        CzyZaładowanoPlik = true;
+                        UtwórzBazęZplikuExcel(openFile);
+
+                        cboArkusze.Items.Clear();       // wyczyść listę wyboru arkuszy
+
+                        UzupełnijListęWyboruArkuszy();
+                    }
+                    catch (IOException ex)
+                    {
+                        MessageBox.Show("Nie można otworzyć pliku poniewaqż jest otwarty\n" + ex.Message);
+                    }
+                    
+                }
+            }
+        }
+        /// <summary>
+        /// Uzupełni listę w której można wybrać arkusz do wyświetlania w datagridView
+        /// </summary>
+        private void UzupełnijListęWyboruArkuszy()
+        {
+            foreach (DataTable dt in wynik.Tables)
+            {
+                cboArkusze.Items.Add(dt.TableName);
+            }
+        }
+        /// <summary>
+        /// Używa biblioteki ExcelDataREader do pobrania danych z pliku Excela do obiektu DataSet
+        /// </summary>
+        /// <param name="openFile">openFileDialog potrzebny do określenia rodzaju wybranego filtra i w zależności od niego zastosowano rodzaj pobierania danych</param>
+        private void UtwórzBazęZplikuExcel(OpenFileDialog openFile)
+        {
+            FileStream fs = File.Open(openFile.FileName, FileMode.Open, FileAccess.Read);
+            IExcelDataReader czytajExcela;
+            if (openFile.FilterIndex == 1)
+                czytajExcela = ExcelReaderFactory.CreateBinaryReader(fs);
+            else
+                czytajExcela = ExcelReaderFactory.CreateOpenXmlReader(fs);
+            czytajExcela.IsFirstRowAsColumnNames = true;
+            wynik = czytajExcela.AsDataSet();
+            czytajExcela.Close();
+        }
+        /// <summary>
+        /// Zdarzenie po zmianie arkusza z listy, zmieniamy tabelę do wyświetlenia
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cboArkusze_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = wynik.Tables[cboArkusze.SelectedIndex]; 
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GłównaMetoda();
+        }
+        private void GłównaMetoda()
+        {
+            List<EnumerableRowCollection<DataRow>> listaWierszy = new List<EnumerableRowCollection<DataRow>>();
+            List<DataTable> tabele = new List<DataTable>();
+            double nrPesel;
+
+            if(double.TryParse(textBox_pesel.Text, out nrPesel))
+            {
+                PobierzNazwyArkuszy(tabele);
+                tabele.RemoveAt(0); // w tej wersji usuń pierwszy arkusz, ponieważ nie jest do sprawdzania danych
+
+                WyfiltrujDaneWgPesel(tabele, listaWierszy, nrPesel);
+
+                rtbox_wyfiltrowaneDane.Clear(); // wyszyć dane z richtextbox
+                try
+                {
+                    lbl_danePacjenta.Text = "Pacjent " + listaWierszy.ElementAt(1).First().ItemArray.ElementAt(1) + " był na badaniach:";
+                    rtbox_wyfiltrowaneDane.AppendText("Pacjent " + listaWierszy.ElementAt(1).First().ItemArray.ElementAt(1) + " był na badaniach:\n");
+                }
+                catch (Exception)
+                {
+                    lbl_danePacjenta.Text = "Brak pacjenta w bazie lub błędny pesel";
+
+                }
+                
+                
+
+                WypiszDaneObadaniach(listaWierszy);
+            }
+
+           
+        }
+
+        /// <summary>
+        /// Pobiera nazwy wszystkich arkuszy zawartych w pliku Excela
+        /// </summary>
+        /// <param name="tabele">Przekazana lista do uzupełnienia o nazwy arkuszy</param>
+        private void PobierzNazwyArkuszy(List<DataTable> tabele)
+        {
+            foreach (DataTable nazwyArkuszy in wynik.Tables)
+            {
+                tabele.Add(nazwyArkuszy);
+            }
+        }
+        /// <summary>
+        /// Filtruje dane za pomocą Linq i dodaje wiersze do listy 
+        /// </summary>
+        /// <param name="tabele"></param>
+        /// <param name="listaWierszy"></param>
+        private static void WyfiltrujDaneWgPesel(List<DataTable> tabele, List<EnumerableRowCollection<DataRow>> listaWierszy, double nrPesel)
+        {
+            foreach (DataTable dt in tabele)
+            {
+                EnumerableRowCollection<DataRow> wiersz = from dane in dt.AsEnumerable()
+                                                          where dane.Field<double>(0) == nrPesel
+                                                          select dane;
+                listaWierszy.Add(wiersz);
+            }
+        }
+
+        private void WypiszDaneObadaniach(List<EnumerableRowCollection<DataRow>> listaWierszy)
+        {
+            for (int i = 0; i < listaWierszy.Count; i++)
+            {
+                foreach (var item in listaWierszy.ElementAt(i))
+                {
+                    rtbox_wyfiltrowaneDane.AppendText(item.ItemArray.ElementAt(2).ToString().Substring(1, 10) + " nr badania: " + item.ItemArray.ElementAt(4) + "\n");
+                }
+            }
+        }
+        private void opisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pierwszą rzeczą jest wskazanie bazy z pliku excela. Wgrywanie bazy w zależności od pliku może trochę trwać.\nNastępnie można w rozwijanej liście obejrzeć dane wg lat.\nAby poszukać po numerze pesel należe tenże numer wprowadzić do okna textBox i nacisnąć przycisk enter lub wcisnąć guzik szukaj...", "Krótki opis");
+        }
+
+        private void textBox_pesel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                GłównaMetoda();
+            }
+        }
+
+        private void autorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Aplikacja darmowa przygotowana przez:\nKamil Pawłowski\nPiotr Pawłowski", "Autorzy");
+        }
+
+        private void zakończToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
